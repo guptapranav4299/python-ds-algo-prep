522bf1b8ea87beb6eb1affa43e1cc11d154de583
Pranav Gupta <guptapranav4299@gmail.com>
2022-07-30 01:10:52 +0530

    added 2 dp questions

--- dynamic_programming/basic_dp_questions.py
+++ dynamic_programming/basic_dp_questions.py
@@ -22,11 +22,62 @@ def fibonacci_tabular(n):
 
     return fib[n]
 
+"""
+You are given a positive integer N . You have to reduce it by performing below operations:
+1. reduce it to n - 1
+2. if it is divisible by 2 then divide by 2
+3. if it is divisible by 3 then divide 3
+Find min operations
+"""
+
+def reduce_n_greedy(n):
+    operations = 0
+    while n >1:
+        if n % 3 == 0:
+            n = n // 3
+            operations += 1
+        elif n % 2 == 0:
+            n = n // 2
+            operations += 1
+        else:
+            n = n - 1
+            operations += 1
+
+    return operations
+
+def reduce_n_greedy_recursive(n):
+    if n == 1:
+        return 0
+    ans = 10000001
+    if n % 2 == 0:
+        ans = min(ans, reduce_n_greedy_recursive(n //2))
+    if n % 3 == 0:
+        ans = min(ans, reduce_n_greedy_recursive(n//3))
+    ans = min(ans, reduce_n_greedy_recursive(n - 1))
+    ans += 1
+
+    return ans
+
+# global dp
+# def reduce_n_greedy_dp_memoize(n):
+#     if n == 1:
+#         return 0
+#     ans = 10000001
+#     if n % 2 == 0:
+#         ans = min(ans, reduce_n_greedy_recursive(n //2))
+#     if n % 3 == 0:
+#         ans = min(ans, reduce_n_greedy_recursive(n//3))
+#     ans = min(ans, reduce_n_greedy_recursive(n - 1))
+#     ans += 1
+#
+#     return ans
 
 if __name__ == "__main__":
     n = 100
     # memo = [-1 for i in range(n+1)]
     # print("Fibonacci number----->",fibonacci_memoization(n))
     # print("Function Calls---->",count)
-
-    print(fibonacci_tabular(n))
+    # print(fibonacci_tabular(n))
+    # print(reduce_n_greedy(10))
+    print(reduce_n_greedy_recursive(10))
+    dp = [-1 for i in range(n + 1)]
--- dynamic_programming/supw.py
+++ dynamic_programming/supw.py
@@ -1,5 +1,5 @@
 """
-Problem
+Problem - 1
 Zonal Computing Olympiad 2014, 30 Nov 2013
 
 In ICO School, all students have to participate regularly in SUPW. There is a different SUPW activity each day, and each activity has its own duration. The SUPW schedule for the next term has been announced, including information about the number of minutes taken by each activity.
@@ -56,6 +56,67 @@ There is only one subtask worth 100 marks. In all inputs:
 """
 
 
+"""
+Problem 2
+Zonal Computing Olympiad 2014, 30 Nov 2013
+
+In IPL 2025, the amount that each player is paid varies from match to match. The match fee depends on the quality of opposition, the venue etc.
+
+The match fees for each match in the new season have been announced in advance. Each team has to enforce a mandatory rotation policy so that no player ever plays three matches in a row during the season.
+
+Nikhil is the captain and chooses the team for each match. He wants to allocate a playing schedule for himself to maximize his earnings through match fees during the season.
+
+Input format
+
+Line 1: A single integer N, the number of games in the IPL season.
+
+Line 2: N non-negative integers, where the integer in position i represents the fee for match i.
+
+Output format
+
+The output consists of a single non-negative integer, the maximum amount of money that Nikhil can earn during this IPL season.
+
+Sample Input 1
+
+5 
+10 3 5 7 3 
+
+
+Sample Output 1
+
+23
+
+
+(Explanation: 10+3+7+3)
+
+Sample Input 2
+
+8
+3 2 3 2 3 5 1 3
+
+
+Sample Output 2
+
+17
+
+
+(Explanation: 3+3+3+5+3)
+
+Test data
+
+There is only one subtask worth 100 marks. In all inputs:
+
+    • 1 ≤ N ≤ 2×105
+
+    • The fee for each match is between 0 and 104, inclusive.
+
+
+Live evaluation data
+
+There are 12 test inputs on the server during the exam.
+"""
+
+
 class SUPW:
     @classmethod
     def min_assignments_dp_tabular(cls, arr, n):
@@ -66,11 +127,13 @@ class SUPW:
 
         for i in range(3, n):
             dp[i] = min([dp[i - 1], dp[i - 2], dp[i - 3]]) + arr[i]
-        print(dp)
         return min([dp[n - 1], dp[n - 2], dp[n - 3]])
 
 if __name__ == "__main__":
     obj = SUPW()
     n = 10
     arr = [3,2,1,1,2,3,1,3,2,1]
-    print(obj.min_assignments_dp_tabular(arr, n))
+    supw = obj.min_assignments_dp_tabular(arr, n)
+    print(supw)
+    ipl_solution = sum(arr) - supw
+    print(ipl_solution)
