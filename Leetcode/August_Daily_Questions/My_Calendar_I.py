d25b0f943a8d6554a2e3cefe3cff09b715c20c40
Pranav Gupta <guptapranav4299@gmail.com>
2022-08-04 01:36:01 +0530

    added subset problem in dp and daily challenge

---
+++ Leetcode/August_Daily_Questions/My_Calendar_I.py
@@ -0,0 +1,54 @@
+"""
+729. My Calendar I
+
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+Implement the MyCalendar class:
+    MyCalendar() Initializes the calendar object.
+    boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+
+
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+
+
+
+Constraints:
+
+    0 <= start < end <= 109
+    At most 1000 calls will be made to book.
+
+
+"""
+
+
+class MyCalendar:
+
+    def __init__(self):
+        self.dates = []
+
+    def book(self, start: int, end: int) -> bool:
+        for previous_start, previous_end in self.dates:
+            if previous_start < end and start < previous_end:
+                return False
+        self.dates.append((start,end))
+        return True
+
+
+if __name__ == "__main__":
+    obj = MyCalendar()
+    print(obj.book(10,20))
+    print(obj.book(15,30))
--- dynamic_programming/2-D-DP/subset_problem.py
+++ dynamic_programming/2-D-DP/subset_problem.py
@@ -1,3 +1,12 @@
+"""
+Given sum and an array of non negative numbers. Determine if subset of array exists if sum of subsets
+equal to the given sum.
+
+arr = [1,2,3,4]
+sum = 6
+"""
+
+
 from itertools import combinations
 
 
@@ -14,9 +23,122 @@ class Solution:
 
         return False
 
+    @classmethod
+    def subset_problem_recursive(cls, arr, index, given_sum):
+        if index == -1:
+            return given_sum == 0
+
+        ans = False
+
+        # include arr[x]
+        if given_sum >= arr[index]:
+            ans |= cls.subset_problem_recursive(arr, index - 1, given_sum - arr[index])
+
+        # exclude arr[x]
+        ans |= cls.subset_problem_recursive(arr, index - 1, given_sum)
+
+        return ans
+
+    # time - o(index*sum)
+    @classmethod
+    def subset_problem_memoization(cls, memo, arr, index, given_sum):
+        if index == -1:
+            return given_sum == 0
+
+        if memo[index][given_sum] != -1:
+            return memo[index][given_sum]
+
+        ans = False
+
+        # include arr[x]
+        if given_sum >= arr[index]:
+            ans |= cls.subset_problem_memoization(memo, arr, index - 1, given_sum - arr[index])
+
+        # exclude arr[x]
+        ans |= cls.subset_problem_memoization(memo, arr, index - 1, given_sum)
+
+        memo[index][given_sum] = ans
+        # print(memo)
+        return memo[index][given_sum]
+
+    # Time - 0(index * sum)
+    # Space - 0(index * sum)
+    @classmethod
+    def subset_problem_tabulation(cls,table, arr, index, given_sum):
+        n = len(arr)
+        for i in range(n + 1):
+            table[i][0] = True
+
+        for i in range(1,n + 1):
+            table[0][i] = False
+
+        for i in range(1, n + 1):
+            for j in range(1, given_sum + 1):
+                if j < arr[i - 1]:
+                    table[i][j] = table[i-1][j]
+                if j >= arr[i - 1]:
+                    table[i][j] = table[i-1][j] or table[i - 1][j - arr[i -1]]
+
+        return table[n][given_sum]
+
+    # time - o (index * sum)
+    # space - -o (2* sum)
+    @classmethod
+    def subset_problem_tabulation_space_optimised_l1(cls,table, arr, index, given_sum):
+        n = len(arr)
+        # for i in range(n + 1):
+        #     table[i][0] = True
+        table[0][0] = True
+
+        for i in range(1,n + 1):
+            table[0][i] = False
+
+        for i in range(1, n+1):
+            for j in range(given_sum+1):
+                table[1][j] = table[0][j]
+
+                if j - arr[i - 1] > 0:
+                    table[1][j] |= table[0][j - arr[i]]
+
+            for j in range(given_sum+1):
+                table[0][j] = table[1][j]
+
+        return table[1][given_sum]
+
+    # time - o(index * sum)
+    # space - o(sum)
+    @classmethod
+    def subset_problem_tabulation_space_optimised_l2(cls,table, arr, index, given_sum):
+        n = len(arr)
+        table[0] = True
+
+        for i in range(1,n + 1):
+            table[i] = False
+
+        for i in range(1, n+1):
+            for j in range(given_sum, -1, -1):
+                if j - arr[i - 1] > 0:
+                    table[j] |= table[j - arr[i]]
+
+        return table[given_sum]
+
 
 if __name__ == "__main__":
     obj = Solution()
     arr = [2, 7, 4, 5, 19]
     given_sum = 12
+
     print(obj.subset_problem_brute_force(arr, given_sum))
+    print(obj.subset_problem_recursive(arr,len(arr) - 1,given_sum))
+
+    memo = [[-1 for i in range(given_sum + 1)] for j in range(len(arr))]
+    print(obj.subset_problem_memoization(memo, arr, len(arr) - 1, given_sum))
+
+    table = [[False for i in range(given_sum + 1) ] for j in range(len(arr) + 1)]
+    print(obj.subset_problem_tabulation(table, arr, len(arr) - 1, given_sum))
+
+    table_space = [[False for i in range(given_sum + 1)] for j in range(2)]
+    print(obj.subset_problem_tabulation_space_optimised_l1(table_space, arr, len(arr) - 1, given_sum))
+
+    table_n = [False for i in range(given_sum + 1)]
+    print(obj.subset_problem_tabulation_space_optimised_l2(table_n, arr, len(arr) - 1, given_sum))
